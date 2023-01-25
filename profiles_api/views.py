c43bc76b6ecde6046587ae8f26e59826063a4c96
Auner Medina <amedina1@godaddy.com>
2023-01-24 18:01:39 -0600

    Added searching to user profile API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from profiles_api import serializers
 from profiles_api import models
@@ -106,5 +107,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating, creating and updating profiles"""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
-    authentication_clases = (TokenAuthentication, )
-    permission_clases = (permissions.UpdateOwnProfile, )
+    authentication_clases = (TokenAuthentication,)
+    permission_clases = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
